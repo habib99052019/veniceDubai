725674ce32b8b5d478f5f98adf27f9beb4fcc997
habib <ha9.0bib90@gmail.com>
2023-10-11 00:51:20 +0200

    first commit

---
+++ src/app/google-meet/google-meet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-meet',
+  templateUrl: './google-meet.component.html',
+  styleUrls: ['./google-meet.component.css']
+})
+export class GoogleMeetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
